d8a32eee629a16ae122e49734e52831c123dba8f
sfalexrog <git@github.com>
2018-06-14 13:20:52 +0300

    Remove unused import

--- Assets/Scripts/NovelScripts/DialogueTree.cs
+++ Assets/Scripts/NovelScripts/DialogueTree.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
-using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;

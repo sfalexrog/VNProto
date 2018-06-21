75f72bf5d0e4486a77ac5611fb30336d82a5bb25
sfalexrog <sfalexrog@gmail.com>
2018-06-21 09:05:15 +0300

    Add outcome preview visualization

--- Assets/Scripts/CardScripts/CardUiGlue.cs
+++ Assets/Scripts/CardScripts/CardUiGlue.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class CardUiGlue : MonoBehaviour
 {
+	
 
 	private GameObject _purposePanel;
 	private GameObject _answerPanel;
@@ -18,6 +19,17 @@ public class CardUiGlue : MonoBehaviour
 
 	private Text _leftAnswerText;
 	private Text _rightAnswerText;
+
+	private Text _nameText;
+	private Text _questionText;
+
+	private Image _actorImage;
+
+	private Image _familyImage;
+	private Image _friendImage;
+	private Image _coupleImage;
+	private Image _classImage;
+	
 	
 	// Bind to scene objects
 	void Awake()
@@ -39,6 +51,16 @@ public class CardUiGlue : MonoBehaviour
 		
 		_leftAnswerText.text = "Left answer";
 		_rightAnswerText.text = "Right answer";
+
+		_nameText = GameObject.Find("Name Text").GetComponent<Text>();
+		_questionText = GameObject.Find("Question Text").GetComponent<Text>();
+
+		_actorImage = GameObject.Find("Actor Image").GetComponent<Image>();
+
+		_familyImage = GameObject.Find("Family Image").GetComponent<Image>();
+		_friendImage = GameObject.Find("Friend Image").GetComponent<Image>();
+		_coupleImage = GameObject.Find("Couple Image").GetComponent<Image>();
+		_classImage = GameObject.Find("Class Image").GetComponent<Image>();
 		
 		ResetUi();
 		ResetListeners();
@@ -49,6 +71,12 @@ public class CardUiGlue : MonoBehaviour
 		_leftAnswerButton.image.color = Color.white;
 		_rightAnswerButton.image.color = Color.white;
 		_cancelAnswerButton.gameObject.SetActive(false);
+		
+		_familyImage.gameObject.SetActive(false);
+		_friendImage.gameObject.SetActive(false);
+		_coupleImage.gameObject.SetActive(false);
+		_classImage.gameObject.SetActive(false);
+		
 	}
 
 	private void ResetListeners()
@@ -71,6 +99,22 @@ public class CardUiGlue : MonoBehaviour
 		otherButton.onClick.AddListener(delegate { CancelOutcome(); });
 		
 		_cancelAnswerButton.gameObject.SetActive(true);
+		
+		_familyImage.gameObject.SetActive(true);
+		_friendImage.gameObject.SetActive(true);
+		_coupleImage.gameObject.SetActive(true);
+		_classImage.gameObject.SetActive(true);
+		// TODO: add predictions from model
+		var familyScale = Random.value * 4;
+		_familyImage.gameObject.transform.localScale = new Vector3(familyScale, familyScale, familyScale);
+		var friendScale = Random.value * 4;
+		_friendImage.gameObject.transform.localScale = new Vector3(friendScale, friendScale, friendScale);
+		var coupleScale = Random.value * 4;
+		_familyImage.gameObject.transform.localScale = new Vector3(coupleScale, coupleScale, coupleScale);
+		var classScale = Random.value * 4;
+		_classImage.gameObject.transform.localScale = new Vector3(classScale, classScale, classScale);
+
+
 	}
 
 	private void CancelOutcome()
